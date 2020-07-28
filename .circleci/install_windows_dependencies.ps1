037eeb1b8a0c5daf646e19fdaa6afc4ca5f795f7
Aaron Meaney <aaronmeaney@hotmail.com>
2020-07-28 21:27:07 +0100

    Add temp file to be deleted in later commit

--- .circleci/install_windows_dependencies.ps1
+++ .circleci/install_windows_dependencies.ps1
@@ -1,5 +1,7 @@
 if ( Test-Path $Env:ProgramFiles\CMake ) {
 	Write-Output "Skipping CMake Install"
+	Write-Output "Adding CMake to PATH"
+	SETX /M PATH "$($Env:PATH);C:\Program Files\CMake"
 } else {
 	Write-Output "Installing CMake"
 	choco install cmake -y --installargs 'ADD_CMAKE_TO_PATH=User'
