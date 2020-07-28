bb40a9907db91bc60a9e260991460455e40f6ed1
Aaron Meaney <aaronmeaney@hotmail.com>
2020-07-28 21:33:44 +0100

    Delete temp file

--- .circleci/install_windows_dependencies.ps1
+++ .circleci/install_windows_dependencies.ps1
@@ -1,7 +1,10 @@
 if ( Test-Path $Env:ProgramFiles\CMake ) {
 	Write-Output "Skipping CMake Install"
+	
 	Write-Output "Adding CMake to PATH"
-	SETX /M PATH "$($Env:PATH);C:\Program Files\CMake"
+	$oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+	$newpath = “$oldpath;C:\Program File\CMake”
+	Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
 } else {
 	Write-Output "Installing CMake"
 	choco install cmake -y --installargs 'ADD_CMAKE_TO_PATH=User'
